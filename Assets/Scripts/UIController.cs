7bb45ecd153743d80b5d2bd29c1b8a473f78d31f
UnitedHaw <92852291+UnitedHaw@users.noreply.github.com>
2022-07-27 15:39:26 +0300

    activate button

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,21 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
     public Button pauseBtn;
-    public Button shildBtn;
+    public Button resumeBtn;
+    public Button restartBtn;
 
     private void Start()
     {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
-        pauseBtn = root.Q<Button>("pauseBtn");
-        shildBtn = root.Q<Button>("shildBtn");
+        var hud = transform.GetChild(0);
+        var pauseMenu = transform.GetChild(1);
 
-        pauseBtn.clicked += PauseButtonPressed;
-        shildBtn.clicked += ShildButtonPressed;
+        
+
+        VisualElement hudRoot = hud.GetComponent<UIDocument>().rootVisualElement;
+        VisualElement pauseMenuRoot = pauseMenu.GetComponent<UIDocument>().rootVisualElement;
+
+        pauseBtn = hudRoot.Q<Button>("pauseBtn");
+
+        resumeBtn = pauseMenuRoot.Q<Button>("resumeBtn");
+        restartBtn = pauseMenuRoot.Q<Button>("restartBtn");
+
+        Hide(pauseMenu);
+
+
+        pauseBtn.clicked += (() => {
+            Time.timeScale = 0f;
+            Show(pauseMenu);
+            Hide(hud);
+        });
+
+        resumeBtn.clicked += (() => {
+            Time.timeScale = 1f;
+            Show(hud);
+            Hide(pauseMenu);
+        });
+
+        restartBtn.clicked += (() =>
+        {
+            SceneManager.LoadScene(0);
+        });
+
+
+        //VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        //var backgorund = root.Q("background");
+
+        //pauseBtn = root.Q<Button>("pauseBtn");
+        //shildBtn = root.Q<Button>("shildBtn");
+
+        //pauseBtn.clicked += PauseButtonPressed;
+        //shildBtn.clicked += ShildButtonPressed;
     }
 
     private void PauseButtonPressed()
@@ -27,5 +65,13 @@ public class UIController : MonoBehaviour
     {
         Transform pfShild = Resources.Load<Transform>("pfPlayerShild");
         Instantiate(pfShild);
+    }
+    public void Show(Transform transform)
+    {
+        transform.gameObject.SetActive(true);
+    }
+    public void Hide(Transform transform)
+    {
+        transform.gameObject.SetActive(false);
     }
 }
